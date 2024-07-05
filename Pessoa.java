2eebe57caee599df69dde70e31a7c1e92637c51f
Vitória Mattos Borges <162926795+vick1407@users.noreply.github.com>
2024-07-05 11:31:20 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,13 @@
+package aula22;
+
+public final class Pessoa {
+    private String nome;
+    private String sobrenome;
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+}
